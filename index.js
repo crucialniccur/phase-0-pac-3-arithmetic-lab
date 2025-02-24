a005a64b12c2b93c1ec11b00410cae2808a1ecc2
Mati Joseph <crucialniccur@gmail.com>
2025-02-24 22:55:38 +0300

    define the return statements for the increment and decrement functions and the code needed

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ function divide(a, b) {
   return a / b;
 }
 
-function increment(n) {}
+function increment(n) {
+  n++;
+  return n;
+}
 
-function decrement(n) {}
+function decrement(n) {
+  n--;
+  return n;
+}
